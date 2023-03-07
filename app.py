b120ad101bce9570d989698c5c137a6538906e56
samc5 <scowan30@stuy.edu>
2023-03-06 23:07:03 -0500

    live updating w/ turbo_flask! Ty Miguel Grinberg!

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import requests
 from humanize import number
-from flask import Flask, render_template, request, Response, session, redirect
-from turbo_flask import Turbo
 import threading
 import time
+from flask import Flask, render_template, request, Response, session, redirect
+from turbo_flask import Turbo
+
 app = Flask(__name__)
 turbo = Turbo(app)
+
 app.secret_key = "6gBvzKwE8RWOt6amHzNz"
 
 #def 
@@ -22,15 +24,8 @@ def check_bases(base_array):
         return "no-one on"
     return output
 
-@app.before_first_request
-def before_first_request():
-    pass
-    #threading.Thread(target=?).start()
-
-@app.route("/", methods = ["POST", "GET"])
-def display():
-    #r = requests.get("https://push.gamechanger.io/push/game/629688bbf99f001df3000789/stream/629688bbf996007c590007a3?index=0&sabertooth_aware=true")
-    
+@app.context_processor
+def requester():
     r = requests.get("https://push.gamechanger.io/push/game/64066eb0d12b5e9d6600000c/stream/64066eb0d12b5e9de2000010?index=0&sabertooth_aware=true")
     data = r.json()
     #print(data['game']['accounts'][0])
@@ -63,13 +58,26 @@ def display():
         half_inning = "top"
     else:
         half_inning = "bottom"
-
-    print(f'The score is {home_score} to {away_score} in the {half_inning} of the {inning_suffix} inning, the count is {balls}-{strikes}, with {outs} outs.\n{pitcher_first} {pitcher_last} is facing hitter {batter_first} {batter_last}')
-    return render_template("index.html", base_text = base_text, home=home_score,away=away_score,half=half_inning,inning=inning,balls=balls,strikes=strikes,outs=outs,pitcher_first=pitcher_first,pitcher_last=pitcher_last,batter_first=batter_first,batter_last=batter_last)
+    return {'base_text':base_text, 'home':home_score,'away':away_score,'half':half_inning,'inning':inning,'balls':balls,'strikes':strikes,'outs':outs,'pitcher_first':pitcher_first,'pitcher_last':pitcher_last,'batter_first':batter_first,'batter_last':batter_last}
 
 
 
+@app.route("/", methods = ["POST", "GET"])
+def display():
+    return render_template("index.html")
 
-if __name__ == "__main__":
-    app.debug = True
-    app.run()
+
+
+def update_load():
+    with app.app_context():
+        while True:
+            time.sleep(5)
+            turbo.push(turbo.replace(render_template('scoreboard.html'), 'sb'))
+
+th = threading.Thread(target=update_load)
+th.daemon = True
+th.start()
+
+# if __name__ == "__main__":
+#     app.debug = True
+#     app.run()
