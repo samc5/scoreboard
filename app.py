07817457a993a3d8b4173c160c0459e172a1cd55
samc5 <scowan30@stuy.edu>
2023-03-14 23:17:04 -0400

    Live scoreboard, formatted semi-nice, admin code can change game/stream urls. Instantly changes. Bases work too which is cool

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from humanize import number
 import threading
 import time
@@ -24,9 +25,22 @@ def check_bases(base_array):
         return "no-one on"
     return output
 
+def color_bases(base_array):
+    output = []
+    for i in range(3):
+        if base_array[i] == 1:
+            output.append("#EFB21F")
+        else:
+            output.append("#888888")
+    return output
 @app.context_processor
 def requester():
-    r = requests.get("https://push.gamechanger.io/push/game/64066eb0d12b5e9d6600000c/stream/64066eb0d12b5e9de2000010?index=0&sabertooth_aware=true")
+    game_file = open("game.txt", "r")
+    game_url = game_file.read()
+    game_file.close()
+
+    #url = "https://push.gamechanger.io/push/game/64066eb0d12b5e9d6600000c/stream/64066eb0d12b5e9de2000010?index=0&sabertooth_aware=true"
+    r = requests.get(game_url)
     data = r.json()
     #print(data['game']['accounts'][0])
 
@@ -41,10 +55,20 @@ def requester():
     inning = data['game']['accounts'][0]['state']['inning']
     inning_suffix = number.ordinal(inning)
 
-    pitcher_first = data['game']['accounts'][0]['state']['pitcher']['first_name']
-    pitcher_last = data['game']['accounts'][0]['state']['pitcher']['last_name']
-    batter_first = data['game']['accounts'][0]['state']['batter']['first_name']
-    batter_last = data['game']['accounts'][0]['state']['batter']['last_name']
+    pitcher_first = "No pitcher"
+    pitcher_last = "No pitcher"
+    batter_first = "No batter"
+    batter_last = "No batter"
+
+    json_string = json.dumps(data)
+    data = json.loads(json_string)
+    if data['game']['accounts'][0]['state']['pitcher'] != None:
+        pitcher_first = data['game']['accounts'][0]['state']['pitcher']['first_name']
+        pitcher_last = data['game']['accounts'][0]['state']['pitcher']['last_name']
+    if data['game']['accounts'][0]['state']['batter'] != None:
+        batter_first = data['game']['accounts'][0]['state']['batter']['first_name']
+        batter_last = data['game']['accounts'][0]['state']['batter']['last_name']
+
     base_array = [0,0,0]
     bases_unclear = data['game']['accounts'][0]['state']['bases']
     
@@ -53,20 +77,40 @@ def requester():
         base_last = i["player"]["last_name"]
         base_array[base-1] = 1
     base_text = check_bases(base_array)
+    base_colors = color_bases(base_array)
     half = data['game']['accounts'][0]['state']['half']
     if half == 0:
         half_inning = "top"
     else:
         half_inning = "bottom"
-    return {'base_text':base_text, 'home':home_score,'away':away_score,'half':half_inning,'inning':inning,'balls':balls,'strikes':strikes,'outs':outs,'pitcher_first':pitcher_first,'pitcher_last':pitcher_last,'batter_first':batter_first,'batter_last':batter_last}
+    print(base_colors)
+    return {'base_colors':base_colors,'base_text':base_text, 'home':home_score,'away':away_score,'half':half_inning,'inning':inning,'balls':balls,'strikes':strikes,'outs':outs,'pitcher_first':pitcher_first,'pitcher_last':pitcher_last,'batter_first':batter_first,'batter_last':batter_last}
 
 
 
 @app.route("/", methods = ["POST", "GET"])
 def display():
-    return render_template("index.html")
+    stream_file = open("stream.txt", "r")
+    stream_url = stream_file.read()
+    print(stream_url)
+    stream_file.close()
+    return render_template("index.html", stream = stream_url)
 
-
+@app.route("/input2", methods = ["POST", "GET"])
+def display2():
+    game = request.form["game"]
+    stream = request.form["stream"]
+    code = request.form["code"]
+    #print(f'Game: {game}, stream: {stream}')
+    if code == "3007":
+        stream_file = open("stream.txt", "w")
+        stream_file.write(f'{stream}')
+        stream_file.close()
+        game_file = open("game.txt", "w")
+        game_file.write(f'{game}')
+        game_file.close()
+    #time.sleep(2)
+    return redirect("/")
 
 def update_load():
     with app.app_context():
